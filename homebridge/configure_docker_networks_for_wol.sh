c80d9d811500c4e4e70997fd45e6de9ba4edc86c
David Hutchison <dhutchison86+github@googlemail.com>
2020-06-14 23:45:37 +0100

    Added support for persisting changes

--- homebridge/configure_docker_networks_for_wol.sh
+++ homebridge/configure_docker_networks_for_wol.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
+##############
+#
+# This script will set the required kernel network settings to 
+# allow broadcast traffic to be sent from a docker network
+# to another network.
+# 
+# This sets using both "sysctl -w" as well as configuring a file in
+# "/etc/sysctl.d/" so the change will persist a reboot. 
+#
+##############
+
 # The name of the network the docker container will be connected to
 DOCKER_NETWORK=traefik-backend
+
+# Disable the setting to ignore echo broadcasts
+sudo sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=0
+echo '# Disable the setting to ignore echo broadcasts' | sudo tee /etc/sysctl.d/97-docker-broadcast.conf
+echo 'net.ipv4.icmp_echo_ignore_broadcasts=0' | sudo tee -a /etc/sysctl.d/97-docker-broadcast.conf
+
+# Enabling broadcast forwarding for "all" interfaces, 
+# although it seems this needs used in combination with specific interfaces
+sudo sysctl -w net.ipv4.conf.all.bc_forwarding=1
+echo '# Enabling broadcast forwarding for "all" interfaces' | sudo tee -a /etc/sysctl.d/97-docker-broadcast.conf
+echo 'net.ipv4.conf.all.bc_forwarding=1' | sudo tee -a /etc/sysctl.d/97-docker-broadcast.conf
+
 
 # Find the subnet for the docker network
 subnet=$(docker network inspect "$DOCKER_NETWORK" | jq --raw-output .[0].IPAM.Config[0].Subnet)
@@ -18,6 +41,8 @@ if [ ! -z subnet ]; then
 
         # Check the setting value to check we are substituting correctly
         sudo sysctl -w net.ipv4.conf.${interface}.bc_forwarding=1
+        echo '# Check the setting value to check we are substituting correctly' | sudo tee -a /etc/sysctl.d/97-docker-broadcast.conf
+        echo "net.ipv4.conf.${interface}.bc_forwarding=1" | sudo tee -a /etc/sysctl.d/97-docker-broadcast.conf
     else
         echo "Could not determine the network interface for $subnet"
     fi
@@ -25,8 +50,3 @@ else
     echo "Could not determine the subnet for $DOCKER_NETWORK"
 fi
 
-# Disable the setting to ignore echo broadcasts
-sudo sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=0
-# Enabling broadcast forwarding for "all" interfaces, 
-# although it seems this needs used in combination with specific interfaces
-sudo sysctl -w net.ipv4.conf.all.bc_forwarding=1
